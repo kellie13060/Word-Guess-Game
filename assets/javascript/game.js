ec5b66ca62f8e044cab32ada80ad8cffe8b44415
Kellie Hunsaker <kellie13060@gmail.com>
2019-11-03 00:35:47 -0600

    small tweaks to track changes in wrong guesses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,7 @@
 var thePokemon = ["bulbasaur", "charmander", "squirtle", "pikachu", "oddish", "tangela", "snorlax", "dragonite", "haunter", "lickitung"];
 var theBackgrounds = ["bulba.jpg", "char.jpg", "squirt.jpg", "pika.jpg", "odd.jpg", "tang.jpg", "snor.jpg", "dragon.jpg", "haunt.jpg", "lick.jpg"];
 //Tracking data
-var wins = 0;
+var winsNumber = 0;
 var remainingGuesses = 8;
 var currentWord;
 var lowerCaseWord;
@@ -16,83 +16,77 @@ var wins = document.getElementById("wins");
 var guessesRemaining = document.getElementById("guessesRemaining");
 var userIncorrectGuesses = document.getElementById("userIncorrectGuesses");
 var pokeBack = document.getElementsByTagName("body")[0];
-console.log(pokeBack);
 
 
 //I choose you!
 function assignWord() {
+  clearArray(wrongAnswerArray);
+  clearArray(answerArray);
+  setTimeout('userIncorrectGuesses.textContent = wrongAnswerArray.join("   ");', 100);
+  remainingGuesses = 8;
+  guessesRemaining.textContent = remainingGuesses;
   var randomNumber = Math.floor(Math.random() * thePokemon.length);
   currentWord = thePokemon[randomNumber];
-  console.log(pokemonIndex);
   pokemonIndex = randomNumber;
-  console.log(pokemonIndex);
   wordLength = currentWord.length;
-  console.log(wordLength);
-  console.log(currentWord);
   lowerCaseWord = currentWord.toLowerCase();
-  console.log(lowerCaseWord);
   //determining how many letters the chosen pokemon is 
   for (var i = 0; i < currentWord.length; i++) {
     answerArray[i] = "_";
   }
   //putting spaces bettwen the letters and removing commas
- guessWord.textContent = answerArray.join("   ");
-
+  guessWord.textContent = answerArray.join("   ");
 };
+
+var winner = function () {
+  alert("You Win! Hit 'OK' to play again :)")
+  pokeBack.style.backgroundImage = "url(assets/images/back2.png)";
+  assignWord();
+  winsNumber++;
+  wins.textContent = winsNumber;
+};
+
+var loser = function () {
+  alert("You lose! Team Rocket Wins!");
+  pokeBack.style.backgroundImage = "url(assets/images/rocket.jpg)";
+  assignWord();
+}
+
+var clearArray = function (arrayToClear) {
+  for (var i = 0; i < arrayToClear.length; i++) {
+    arrayToClear.length = 0;
+  }
+}
 
 document.onkeyup = function (event) {
   var userGuess = event.key;
 
-    //first check to see if the letter is on the page
-  if (answerArray.indexOf(userGuess) !== -1 || wrongAnswerArray.indexOf(userGuess) !== -1 ){
-    console.log("hit1");
+  //first check to see if the letter is on the page
+  if (answerArray.indexOf(userGuess) !== -1 || wrongAnswerArray.indexOf(userGuess) !== -1) {
+    alert("Already Guessed That Letter. :)")
 
   } else {
     //if not on the page we are putting on the page where it goes (in one of the arrays)
-    console.log("hit2");
     if (lowerCaseWord.indexOf(userGuess) !== -1) {
-      for (var i=0; i < currentWord.length; i++) {
-        if(currentWord[i] === userGuess) {
+      for (var i = 0; i < currentWord.length; i++) {
+        if (currentWord[i] === userGuess) {
           answerArray[i] = currentWord[i];
           wordLength -= 1;
-          console.log(wordLength);
-          
+          guessWord.textContent = answerArray.join("   ");
+          if (wordLength <= 0) {
+            pokeBack.style.backgroundImage = "url(assets/images/" + theBackgrounds[pokemonIndex] + ")";
+            setTimeout('winner();', 200);
+          }
         }
       }
-      answerArray[lowerCaseWord.indexOf(userGuess)] = currentWord[lowerCaseWord.indexOf(userGuess)];
-      guessWord.textContent = answerArray.join("   ");
-      console.log("letter")
+    } else {
+      wrongAnswerArray.push(userGuess);
+      userIncorrectGuesses.textContent = wrongAnswerArray.join("   ");
+      remainingGuesses--;
+      guessesRemaining.textContent = remainingGuesses;
+      if (remainingGuesses <= 0) {
+        loser();
+      }
     }
   }
 };
-  
-
-  //track how many wins
-//   if (wordLength > 0 && remainingGuesses > 0) {
-//     if (lowerCaseWord.indexOf(event.key) !== -1 && lowerCaseWord.indexOf(event.key) && wrongAnswerArray.indexOf(event.key)) {
-//       console.log(!lowerCaseWord.indexOf(event.key));
-//       for (var i = 0; i <= currentWord.length; i++) {
-//         if (lowerCaseWord[i] === event.key) {
-//           answerArray[i] = currentWord[i];
-//           guessWord.textContent = answerArray.join("   ");
-//           wordLength -= 1;
-//           console.log(wordLength);
-//         };
-//       }; 
-//     } else if (wrongAnswerArray.indexOf(event.key) === -1) {
-//       wrongAnswerArray.push(event.key.toLowerCase());
-//       userIncorrectGuesses.textContent = wrongAnswerArray.join("   ");
-//       console.log(wrongAnswerArray);
-//       remainingGuesses -= 1;
-//       guessesRemaining.textContent = remainingGuesses;
-//       console.log(remainingGuesses);
-//     } else {};
-//   } else if (wordLength <= 1 && remainingGuesses > 0) {
-//     pokeBack.style.backgroundImage = "url(assets/images/" + theBackgrounds[pokemonIndex] + ")";
-//     setTimeout("location.reload(true);", 3000);
-
-//   } else {
-//     alert("You lose! Wait 1 second to start again.");
-//     setTimeout("location.reload(true);", 1000);
-//   };
-// };
